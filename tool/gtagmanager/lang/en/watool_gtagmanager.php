7631cb94839779c36876fe3820a222d24a0995f8
Dmitrii Metelkin <dmitriim@catalyst-au.net>
2018-06-15 14:04:59 +1000

    Add Google Tag Manager code

---
+++ tool/gtagmanager/lang/en/watool_gtagmanager.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Lang strings
+ *
+ * @package   watool_gtagmanager
+ * @author    Dmitrii Metelkin (dmitriim@catalyst-au.net)
+ * @copyright 2018 Catalyst IT
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+$string['pluginname'] = 'Google Tag Manager';
+$string['siteid'] = 'Container ID';
+$string['siteid_help'] = 'Enter your Container ID for Google Tag Manage';
+$string['privacy:metadata'] = 'Google Tag Manager does not store or send any user data';
