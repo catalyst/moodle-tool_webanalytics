989ccaeac97580cc3072cfcae02d3afd8920e67a
Brendan Heywood <brendan@catalyst-au.net>
2026-03-25 13:35:39 +1100

    Fix code standards

--- tool/guniversal/classes/privacy/provider.php
+++ tool/guniversal/classes/privacy/provider.php
@@ -38,8 +38,8 @@ use core_privacy\local\request\userlist;
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class provider implements
-    \core_privacy\local\request\core_userlist_provider,
     \core_privacy\local\metadata\provider,
+    \core_privacy\local\request\core_userlist_provider,
     \core_privacy\local\request\plugin\provider {
     /**
      * Returns information about the user data stored in this component.
