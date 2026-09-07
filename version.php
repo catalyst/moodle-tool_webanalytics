02338717233d101cb923c92c9434c66ad9255775
Jay Oswald <jozzie52@gmail.com>
2026-09-07 14:11:20 +1000

    bump + flag 502 support

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2024101801;      // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release   = 2024101801;      // Same as version.
+$plugin->version   = 2024101802;      // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release   = 2024101802;      // Same as version.
 $plugin->requires  = 2022112800;      // Requires Moodle 4.1 or later.
 $plugin->component = "tool_webanalytics";
-$plugin->supported = [401, 405];  // Available as of Moodle 4.1.0 or later.
+$plugin->supported = [401, 502];  // Available as of Moodle 4.1.0 or later.
 $plugin->maturity  = MATURITY_STABLE;
